ef61ff3c6e242d06948199b436fc53baf8c2841e
ooak <ooak@users.noreply.github.com>
2025-10-10 23:44:50 +0300

    Add Facebook Pixel via index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,25 @@
+// Facebook Pixel
+(function(f: any,b: any,e: any,v: any,n?: any,t?: any,s?: any){
+  if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)
+})(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+declare global {
+  interface Window {
+    fbq: any;
+  }
+}
+
+window.fbq('init', '844531991375402');
+window.fbq('track', 'PageView');
+
+import React from 'react';
+import './index.css';
+// ... rest of your imports
 import React from 'react';
 import './index.css';
 import { render } from 'react-dom';
