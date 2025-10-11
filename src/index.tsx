b5a23a71483291f2d0a41317553a8a2caa6b26d3
ooak <ooak@users.noreply.github.com>
2025-10-11 16:48:26 +0300

    Fix: Update to correct Facebook Pixel ID 1734437306869993

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,29 @@
-// Facebook Pixel
-(function(f: any,b: any,e: any,v: any,n?: any,t?: any,s?: any){
-  if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)
-})(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+// Facebook Pixel Code
+(function(f: any, b: any, e: string, v: string) {
+  const n: any = f.fbq = function() {
+    n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments);
+  };
+  if (!f._fbq) f._fbq = n;
+  n.push = n;
+  n.loaded = true;
+  n.version = '2.0';
+  n.queue = [];
+  const t = b.createElement(e);
+  t.async = true;
+  t.src = v;
+  const s = b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t, s);
+})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
 
-declare global {
-  interface Window {
-    fbq: any;
-  }
-}
+(window as any).fbq('init', '1734437306869993');
+(window as any).fbq('track', 'PageView');
 
-window.fbq('init', '1734437306869993');
-window.fbq('track', 'PageView');
-
-import React from 'react';
-import './index.css';
-// ... rest of your imports
 import React from 'react';
 import './index.css';
 import { render } from 'react-dom';
 import { AppRouter } from './AppRouter';
+
+render(<AppRouter />, document.getElementById('root'));
+import { AppRouter } from './AppRouter';
+
 render(<AppRouter />, document.getElementById('root'));
