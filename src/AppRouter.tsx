35da464544191238bbc00e52ce005002aa5373ac
ooak <ooak@users.noreply.github.com>
2025-10-18 16:24:35 +0300

    Add Google Tag Manager

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { App } from './App';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { AdminPage } from './pages/AdminPage';
+import React, { useEffect } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { App } from './App'
+import { CheckoutPage } from './pages/CheckoutPage'
+import { AdminPage } from './pages/AdminPage'
+
 export function AppRouter() {
-  return <BrowserRouter>
+  useEffect(() => {
+    // Add GTM noscript to body
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KRHZ843T" height="0" width="0" style="display:none;visibility:hidden"></iframe>';
+    document.body.insertBefore(noscript, document.body.firstChild);
+  }, []);
+
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/checkout" element={<CheckoutPage />} />
         <Route path="/admin" element={<AdminPage />} />
       </Routes>
-    </BrowserRouter>;
+    </BrowserRouter>
+  )
 }
