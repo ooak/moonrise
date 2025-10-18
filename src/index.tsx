c0e1fdd89484631bad25b23ece45c453487fed6f
ooak <ooak@users.noreply.github.com>
2025-10-18 16:30:47 +0300

    Fix GTM code - remove HTML tags

--- src/index.tsx
+++ src/index.tsx
@@ -17,15 +17,17 @@
 
 (window as any).fbq('init', '1165039268822754');
 (window as any).fbq('track', 'PageView');
-
 // Google Tag Manager
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KRHZ843T');</script>
-<!-- End Google Tag Manager -->
+(function(w: any, d: any, s: string, l: string, i: string) {
+  w[l] = w[l] || [];
+  w[l].push({'gtm.start': new Date().getTime(), event: 'gtm.js'});
+  var f = d.getElementsByTagName(s)[0],
+  j = d.createElement(s) as any, 
+  dl = l != 'dataLayer' ? '&l=' + l : '';
+  j.async = true;
+  j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+  f.parentNode.insertBefore(j, f);
+})(window, document, 'script', 'dataLayer', 'GTM-KRHZ843T');
 
 import React from 'react';
 import './index.css';
